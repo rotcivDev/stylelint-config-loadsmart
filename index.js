7b7600b68c7e6ebc9637fc2d418ff30c08b513ac
Felipe Pereira <felipe.pereira@loadsmart.com>
2021-03-16 12:40:58 -0300

    add ignoreFiles config

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 module.exports = {
     "plugins": ["stylelint-no-unsupported-browser-features", "stylelint-order"],
     "extends": ["stylelint-config-recommended", "stylelint-config-rational-order", "stylelint-config-prettier"],
+    "ignoreFiles": [
+        "**/*.snap",
+        "**/__snapshots__/**/*",
+        "node_modules/",
+        "**/*.json"
+    ],
     "rules": {
         "order/order": [
             "dollar-variables",
