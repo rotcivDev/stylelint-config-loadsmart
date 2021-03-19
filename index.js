cd668af7c6dfb7e5010e06321671f3ab1820a086
Felipe Pereira <felipe.pereira@loadsmart.com>
2021-03-19 15:52:47 -0300

    add eslint and config for it

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "plugins": ["stylelint-no-unsupported-browser-features", "stylelint-order"],
-    "extends": ["stylelint-config-recommended", "stylelint-config-rational-order", "stylelint-config-prettier"],
-    "ignoreFiles": [
-        "**/*.snap",
-        "**/__snapshots__/**/*",
-        "node_modules/",
-        "**/*.json"
+  plugins: ['stylelint-no-unsupported-browser-features', 'stylelint-order'],
+  extends: ['stylelint-config-recommended', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
+  ignoreFiles: [
+    '**/*.snap',
+    '**/__snapshots__/**/*',
+    'node_modules/',
+    '**/*.json'
+  ],
+  rules: {
+    'order/order': [
+      'dollar-variables',
+      'declarations'
     ],
-    "rules": {
-        "order/order": [
-            "dollar-variables",
-            "declarations"
-        ],
-        "plugin/no-unsupported-browser-features": [true, {
-            "severity": "warning",
-            "ignorePartialSupport": true
-        }],
-        "plugin/rational-order": [true, {
-            "border-in-box-model": false,
-            "empty-line-between-groups": true
-        }]
-    }
+    'plugin/no-unsupported-browser-features': [true, {
+      severity: 'warning',
+      ignorePartialSupport: true
+    }],
+    'plugin/rational-order': [true, {
+      'border-in-box-model': false,
+      'empty-line-between-groups': true
+    }]
+  }
 }
